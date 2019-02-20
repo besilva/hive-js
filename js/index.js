597d3af1c15257307b94bcd581fbd35b10d3205f
Bernardo Silva <bernardo.silva@10-40-40-19.0101.ecps.br.iosda.org>
2019-02-20 17:02:04 -0300

    initial structure

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const titulo = "Javascript: deus me livre mas quem me dera";
+const autor = "Bernardo Silva";
+
+const h1_titulo = document.querySelector("#titulo");
+const autor_span = document.querySelector("#autor");
+const text  = document.querySelector("#text");
+h1_titulo.textContent = titulo;
+autor_span.textContent = autor;
+text.value = titulo;
+//alert("teste");
